25937d867dcf4d257736c8e369436a488aa94b4a
BAANG <chcelestino@gmail.com>
2019-06-24 14:42:45 -0700

    Typo

--- Letter.js
+++ Letter.js
@@ -4,4 +4,18 @@ var Letter = function(value) {
     this.value = value;
     this.isGuessed = false;
 
+    this.correctGuess = function (char) {
+        if (this.value === " ") {
+            this.guessed = true;
+            return " ";
+        }
+        if (this.isGuessed === char) {
+            this.isGuessed = true;
+            return this.value
+        }
+    }
+
+    this.checkGuess = function (character) {
+        if ()
+    }
 }
