cfe03f154979d29d54edaa2bacf9e453cf51721d
BAANG <chcelestino@gmail.com>
2019-06-24 18:49:47 -0700

    Create array for storing current letters in selected word

--- Word.js
+++ Word.js
@@ -1,1 +1,12 @@
 var Letter = require('./Letter.js')
+
+var Word = function(currentWord) {
+    this.letters = []; //Array containing each of the individual letter objects for current word.
+    
+    for (var i = -0; i < currentWord.length; i ++) { // Constructs letter object for each letter in current word and stores in array.
+        var newLetter = new Letter(currentWord[i]);
+        letters.push(newLetter);
+    }
+
+    
+}
