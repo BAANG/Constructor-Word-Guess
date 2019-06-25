eee3e66247b2028b06fde386b8b5b2ab0c58e09d
BAANG <chcelestino@gmail.com>
2019-06-24 18:38:28 -0700

    Letter.js completed

--- Letter.js
+++ Letter.js
@@ -4,18 +4,22 @@ var Letter = function(value) {
     this.value = value;
     this.isGuessed = false;
 
-    this.correctGuess = function (char) {
+    this.checkLetter = function (char) { // Checks if letter in string is a space, or if the letter has beeng guessed already. (Returns the correct value to be printed)
         if (this.value === " ") {
             this.guessed = true;
             return " ";
-        }
-        if (this.isGuessed === char) {
-            this.isGuessed = true;
-            return this.value
+        } else if (!this.isGuessed) { // If value/letter is not yet guessed, return underscore to signify an unsolved letter.
+            return "_";
+        } else {
+            return this.value;
         }
     }
 
-    this.checkGuess = function (character) {
-        if ()
+    this.checkGuess = function(userGuess) {
+        if (userGuess === this.value) {
+            this.isGuessed = true;
+        }
     }
-}
+};
+
+module.exports = Letter;
