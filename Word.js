e79c51dd1f3f2b2a624984f586a03593c4f312c1
BAANG <chcelestino@gmail.com>
2019-06-24 23:51:06 -0700

    Functional game minus completed restart function

--- Word.js
+++ Word.js
@@ -1,19 +1,23 @@
 var Letter = require('./Letter.js')
 
 var Word = function(currentWord) {
+    //checkWord will be compared to check win condition
+    this.correctWord = currentWord;
+    
     this.letters = []; //Array containing each of the individual letter objects for current word.
     
     for (var i = 0; i < currentWord.length; i ++) { // Constructs letter object for each letter in current word and stores in array.
         var newLetter = new Letter(currentWord[i]);
         this.letters.push(newLetter);
     }
+
     
     this.returnString = function() { // Checks each letter object and concatenates either a space, an underscore or the letter depending on the conditions set/returned by checkLetter function
         this.printString = "";
         for (var i = 0; i < this.letters.length; i ++) {
            this.printString += (this.letters[i].checkLetter()) + " "
         }
-        console.log(this.printString)
+        console.log("\n" + this.printString)
     }
 
     this.userGuess = function(userInput) { // Compares user input to each of the stored letter objects in array
--- index.js
+++ index.js
@@ -7,29 +7,44 @@
     var guessWord;
     var alphabet = "abcdefghijklmnopqrstuvwxyz"
     var guessedLetters = [];
-    var guessesLeft = 12;
-
+    var guessesLeft = 15;
+    var correctGuess = [];
+    var correctLetters = [];
     
-    inquirer.prompt([
-        {
-            type: 'confirm',
-            name: 'gameStart',
-            message: "Ready to play some no-holds-barred random word guessing game?!"
+    
+    function playGame() {
+        generateWord = randomWords();
+        storeSolution();
+        function storeSolution () {
+            unsortedArray = [];
+            for (var i = 0; i < generateWord.length; i ++) {
+                if (!correctLetters.includes(generateWord[i])) {
+                    unsortedArray.push(generateWord[i])
+                }
+            }
+            correctLetters = unsortedArray.sort();
         }
-    ]).then(function(response) {
-        if (response.gameStart) {
-            guessesLeft = 12
-            var generateWord = randomWords()
-            guessWord = new Word(generateWord)
-            console.log("\n")
-            console.log(chalk.magenta("GUESS THIS WORD:"))
-            guessWord.returnString()
-            guessPrompt();
-        }
-    })
+
+        inquirer.prompt([
+            {
+                type: 'confirm',
+                name: 'gameStart',
+                message: "Ready to play some no-holds-barred random word guessing game?!"
+            }
+        ]).then(function(response) {
+            if (response.gameStart) {
+                guessesLeft = 15
+                guessWord = new Word(generateWord)
+                console.log("\n")
+                console.log(chalk.magenta("GUESS THIS WORD:"))
+                guessWord.returnString()
+                checkWin();
+            }
+        })
+    }
 
     function guessPrompt() {
-        console.log("\n")
+        console.log("\nYou have " + chalk.inverse(guessesLeft) + " left...")
         inquirer.prompt([
             {
                 type: 'input',
@@ -39,21 +54,55 @@
         ]).then(function(response) {
             console.log("\n")
             var userInput = response.userGuess.toLowerCase();
-            if (!alphabet.includes(userInput) || guessedLetters.includes(userInput) ||userInput === "" || userInput === " ") {
-                console.log('\nYou must enter a valid letter [A-Z]')
-                console.log("You've guessed: " + guessedLetters + "\n")
-                console.log(chalk.magenta("GUESS THIS WORD:"))
-                guessWord.returnString();
-                guessPrompt();
+
+            if (!alphabet.includes(userInput) || guessedLetters.includes(userInput) ||userInput === "" || userInput === " " || userInput.length > 1) { // Conditions for incorrect guess
+                console.log(chalk.red('\nYou must enter a valid letter [A-Z]'))
+                // console.log("You've guessed: [" + guessedLetters + "]\n")
+                // console.log(chalk.magenta("GUESS THIS WORD:"))
+                // guessWord.returnString();
+                checkWin();
                 
             } else if (userInput.length = 1){ // Pushes user input into array of valid inputs, reduces number of guesses left, and checks if guess was correct
+               if (generateWord.includes(userInput)) {
+                   correctGuess.push(userInput);
+               }
                 guessedLetters.push(userInput);
-                console.log("\nYou've guessed: " + guessedLetters + "\n")
+                console.log("\nYou've guessed: [" + guessedLetters + "]\n")
                 guessesLeft--;
                 guessWord.userGuess(userInput);
                 console.log(chalk.magenta("GUESS THIS WORD:"))
                 guessWord.returnString()
-                guessPrompt();
+                checkWin();
             }
         })
     }
+
+    function checkWin() {
+        correctGuess = correctGuess.sort()
+        // console.log("\n" + correctGuess + " is the correct guess array")
+        // console.log(correctLetters + " is the correct letters array\n")
+
+            if (correctGuess.length === correctLetters.length && guessesLeft > 0) {
+            console.log('############')
+            console.log('############')
+            console.log('##YOU WIN!##')
+            console.log('############')
+            console.log('############\n\n')
+
+            correctLetters = [];
+            playGame();
+        } else if (guessesLeft > 0) {
+            guessPrompt();
+        } else if (guessesLeft = 0) {
+            console.log('#############')
+            console.log('#############')
+            console.log('#YOU LOSE...#')
+            console.log('#############')
+            console.log('#############')
+
+            correctLetters = [];
+            playGame();
+        }
+    }
+
+    playGame();
